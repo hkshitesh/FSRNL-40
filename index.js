d7d729ca4317fc3f8820ac49e2dcb0ad6c653fef
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-12-30 21:48:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const dbConnect= require('./mongodb');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App></App>
-    <App></App>
-    <App></App>
-  </React.StrictMode>
-);
+dbConnect().then((resp)=>{
+resp.find({name:'nord'}).toArray().then((data)=>{
+console.log(data)
+})
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const main=async ()=>{
+   let data = await dbConnect();
+   data = await data.find({name:'nord'}).toArray();
+   console.log(data)
+}
+
+main()
